7474ce3a8e5768a87e640cdd75456df990174b3d
Racheal Soosaipillai <soosaipillai.racheal@gmail.com>
2019-12-05 13:00:01 -0500

    Added 2 events

--- js/popcorn-data-from-google.js
+++ js/popcorn-data-from-google.js
@@ -26,7 +26,7 @@ var loadPopcorn = function(){
     // Instructions are here: https://github.com/jsoma/tabletop#1-publishing-your-google-sheet
     // It's very important, though, to keep the same column headers as in our example spreadsheet:
     // https://docs.google.com/spreadsheets/d/1pL_Lj62_ZcW7iawTCQ_5BQsmdynCtC8y5BCNy3k2LOM/
-    let public_spreadsheet_key = 'https://docs.google.com/spreadsheets/d/1pL_Lj62_ZcW7iawTCQ_5BQsmdynCtC8y5BCNy3k2LOM/pubhtml?gid=0&single=true';
+    let public_spreadsheet_key = 'https://docs.google.com/spreadsheets/d/1bYB1VzaDy0j0iuRAGDV-jjTIpQKhT2Q6q7u5jT_mDpU/';
 
     // now we are going to use the tabletop.js library, which was called in our 
     // HTML file, to grab the date from the spreadsheet and process it so that 
